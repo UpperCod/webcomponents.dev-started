32097bebee4ea69f418b27d00db25f3549e54e92
Matias Trujillo <mtrujillo@outlook.cl>
2024-04-11 20:01:35 -0400

    workspaces

---
+++ src/index.tsx
@@ -0,0 +1,45 @@
+import { c, useProp } from "atomico";
+
+const MyComponent = c(()=>{
+    let [count, setCount] = useProp("count");
+
+  return (
+    <host shadowDom>
+      <button onclick={() => setCount(count - 1)}>-</button>
+      <span>{count}</span>
+      <button onclick={() => setCount(count + 1)}>+</button>
+    </host>
+  );
+},{
+    props: {
+        count: {
+            type: Number,
+            value: 0,
+          }
+    },
+    styles: css`
+        * {
+        font-size: 200%;
+        }
+    
+        span {
+        width: 4rem;
+        display: inline-block;
+        text-align: center;
+        }
+    
+        button {
+        width: 4rem;
+        height: 4rem;
+        border: none;
+        border-radius: 10px;
+        background-color: seagreen;
+        color: white;
+        }
+    `
+})
+
+
+
+
+customElements.define("my-counter", MyComponent);
